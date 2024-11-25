9520d0e6ee1e09f996c0191cd182ac564a26bace
Nathan Ler <nathan.leroueil@free.fr>
2024-11-25 15:26:26 +0100

    a

---
+++ tp3/tp3.js
@@ -0,0 +1,11 @@
+document.getElementById("btnAjouter").onclick = () => {
+    
+    const my_Tbody = document.getElementById('myTbody');
+
+    const fruit = document.getElementById('fruit').value;
+    document.getElementById('fruit').value=''; 
+
+
+
+
+}
